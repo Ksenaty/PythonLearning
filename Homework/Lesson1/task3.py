5a68abea13dd7610c4ee53d4858444174ae28f46
Yaroslav <73306309+Ksenaty@users.noreply.github.com>
2020-10-23 22:03:37 +0300

    Revert "Lesson1"

--- Homework/Lesson1/task3.py
+++
@@ -1,4 +0,0 @@
-number_n = input('Введите чиcло n>>>')
-summary = int(number_n) + int(number_n*2) + int(number_n*3)
-print('Сумма  {0} + {0}*{0} + {0}*{0}*{0} = {1}'.format(number_n, summary))
-print(summary)
